db5784ba6edf7c4ee4e61b8834f1db62c44e3eb1
Super User <root@ip-172-31-31-167.ec2.internal>
2026-01-22 01:11:16 +0000

    Validate format

--- main.tf
+++ main.tf
@@ -1,35 +1,35 @@
 terraform {
-        required_providers {
-                aws = {
-                        source ="hashicorp/aws"
-                        version = "~>5.0"
-                }
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
 }
 
 provider "aws" {
-        region = "us-east-1"
+  region = "us-east-1"
 }
 
 data "aws_security_group" "existing_sg" {
-        id = "sg-0c51cf8e3efe6cd32"
+  id = "sg-0c51cf8e3efe6cd32"
 }
 
 resource "aws_instance" "demo" {
-        ami = "ami-0c02fb55956c7d316"
-        instance_type ="t3.micro"
-        key_name = aws_key_pair.reused_key.key_name
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t3.micro"
+  key_name      = aws_key_pair.reused_key.key_name
 
-        vpc_security_group_ids = [
-                data.aws_security_group.existing_sg.id
-        ]
+  vpc_security_group_ids = [
+    data.aws_security_group.existing_sg.id
+  ]
 
-        tags = {
-                Name = "terraform-ec2-demo"
-        }
+  tags = {
+    Name = "terraform-ec2-demo"
+  }
 }
 
 resource "aws_key_pair" "reused_key" {
-        key_name = "reused-key"
-        public_key = file("/var/lib/jenkins/.ssh/terraform-key.pub")
+  key_name   = "reused-key"
+  public_key = file("/var/lib/jenkins/.ssh/terraform-key.pub")
 }
